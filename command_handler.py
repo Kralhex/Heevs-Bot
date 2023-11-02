329446ab973ab709b482c5851b110f111cb70666
Kralhex <kralhex31@gmail.com>
2023-11-02 18:14:42 +0300

    Create command_handler.py

---
+++ command_handler.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+from commands import delete_messages, who, ping, team_up
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+intents.guilds = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+bot.add_command(ping.ping)
+bot.add_command(delete_messages.delete_limited)
+bot.add_command(delete_messages.delete_all)
+bot.add_command(delete_messages.delete_mine)
+bot.add_command(delete_messages.delete_mine_limited)
+bot.add_command(who.whoareyou)
+bot.add_command(who.whoami)
+bot.add_command(who.whois)
+bot.add_command(team_up.team_me)
+
+@bot.event
+async def on_ready():
+	print(f"This is {bot.user} with {bot.user.id} ID.")
+
+@bot.event
+async def on_message_edit(before, after):
+	msg = f"**{before.author}** edited their message:\n{before.content} -> {after.content}"
+	print(msg)
+	await before.channel.send(msg)
